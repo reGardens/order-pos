76f9c0f63ca2fbfbd6879d8cbe08399ada2d1215
Petani Kode <rezzabagus.rb@gmail.com>
2024-04-28 12:58:43 +0700

    feat: add vue-chartjs

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import './css/main.css'
 import App from './pages/App.vue'
-import { Carousel, initTWE } from "tw-elements";
+import { Carousel, initTWE, Ripple} from "tw-elements";
 
 createApp(App).mount('#app')
-initTWE({ Carousel } );
+initTWE({ Carousel, Ripple });
--- src/pages/Home/Index.vue
+++ src/pages/Home/Index.vue
@@ -7,18 +7,23 @@ defineProps<{
     product: string;
     discount: BigInteger;
     price: number;
+    chartOptions: any;
+    chartData: any;
 }>();
 
 defineEmits(["update:modelValue"]);
 
 const discount = ref("");
 const moreDiscount = ref("");
+const count = ref(1)
 </script>
 
 <template>
-    <main class="px-4">
-        <div class="max-w-screen-sm backdrop-blur-xl bg-[#7FC771]/30 rounded-md p-4 my-6 px-4 mx-auto shadow-xl">
-            <p class="font-bold uppercase text-2xl pb-4 pt-5 tracking-widest">Form Input Natural Farm</p>
+    <main class="px-4 my-6">
+        <div class="max-w-screen-sm backdrop-blur-xl bg-[#7FC771]/30 rounded-md p-4 px-4 mx-auto shadow-xl">
+            <p class="font-bold uppercase text-2xl pb-4 tracking-widest">
+                Form Input Natural Farm
+            </p>
 
             <form action="" method="get" @submit.prevent="">
                 <div class="grid grid-cols-1 md:grid-cols-6 pb-5 items-center gap-2">
@@ -45,48 +50,79 @@ const moreDiscount = ref("");
                     </select>
                     <div class="col-span-2">Discount</div>
                     <div class="col-span-4 flex items-center gap-2">
-                        <input type="number" v-model="discount" class="rounded-md py-2 px-3 outline-none w-full">
+                        <input type="number" v-model="discount" class="rounded-md py-2 px-3 outline-none w-full" />
                         <span class="font-extrabold text-xl">%</span>
+                    </div>
+                    <p class="col-span-2">Quantity</p>
+                    <div class="col-span-4 flex items-center gap-2">
+                        <button type="button" data-twe-ripple-init data-twe-ripple-color="light" @click="count--"
+                            class="inline-block rounded-full bg-primary p-1 uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
+                            </svg>
+                        </button>
+                        <input type="number" v-model="count"
+                            class="rounded-md py-2 px-3 outline-none min-w-10 max-w-20">
+                        <button type="button" data-twe-ripple-init data-twe-ripple-color="light" @click="count++"
+                            class="inline-block rounded-full bg-primary p-1 uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                            </svg>
+                        </button>
                     </div>
                     <div class="col-span-2">Input Discount Tambahan</div>
                     <div class="col-span-4 flex items-center gap-2">
-                        <input type="number" v-model="moreDiscount" class="rounded-md py-2 px-3 outline-none w-full">
+                        <input type="number" v-model="moreDiscount" class="rounded-md py-2 px-3 outline-none w-full" />
                         <span class="font-extrabold text-xl">%</span>
                     </div>
                 </div>
 
-                <div class="mt-5 grid grid-cols-6 gap-2 border-t border-white">
-                    <p class="col-span-6 font-bold uppercase text-2xl pt-4 pb-2 tracking-widest">Order</p>
+                <div class="mt-5 grid grid-cols-6 items-center gap-2 border-t border-white">
+                    <p class="col-span-6 font-bold uppercase text-2xl pt-4 pb-2 tracking-widest">
+                        Order
+                    </p>
                     <p class="col-span-2">Customer</p>
                     <span class="col-span-4 font-semibold md:text-lg">: {{ selectedCustomer }}</span>
                     <p class="col-span-2">Payment</p>
                     <span class="col-span-4 font-semibold md:text-lg">: {{ selectedPayment }}</span>
                     <p class="col-span-2">Product</p>
-                    <span class="col-span-4 font-semibold md:text-lg">: {{ selectedProduct ? selectedProduct.name : ''
-                        }}</span>
+                    <span class="col-span-4 font-semibold md:text-lg">
+                        : {{ selectedProduct ? selectedProduct.name : "" }}
+                    </span>
                     <p class="col-span-2">Harga</p>
                     <span class="col-span-4 font-semibold md:text-lg">
-                        : {{ selectedProduct ? formatCurrency(selectedProduct.price, 'ID', 'IDR') : '' }}
+                        : {{ selectedProduct ? formatCurrency(selectedProduct.price, "ID", "IDR") : "" }}
                     </span>
                     <p class="col-span-2">Discount</p>
-                    <span class="col-span-4 font-semibold md:text-lg">: {{ discount }}%</span>
+                    <span class="col-span-4 font-semibold md:text-lg">: {{ discount }} %</span>
                     <p class="col-span-2">Discount Tambahan</p>
-                    <span class="col-span-4 font-semibold md:text-lg">: {{ moreDiscount }}%</span>
+                    <span class="col-span-4 font-semibold md:text-lg">: {{ moreDiscount }} %</span>
                     <p class="col-span-2">Total</p>
-                    <span class="col-span-4 font-semibold text-lg md:text-xl">
-                        : {{ formatCurrency(calculateTotalWithDiscount(selectedProduct?.price, discount, moreDiscount),
-                            'ID', 'IDR') }}
+                    <span class="col-span-4 font-extrabold text-lg md:text-xl">
+                        : {{ formatCurrency(quantity(calculateTotalWithDiscount(selectedProduct?.price, discount,
+                moreDiscount), count), 'ID', 'IDR') }}
                     </span>
                 </div>
             </form>
         </div>
     </main>
+
+    <section class="p-4 md:!p-10">
+        <Line id="my-chart-id" :options="chartOptions" :data="chartData"
+            class="backdrop-blur-xl bg-[#7FC771]/30 !p-4 rounded-md shadow-xl md:!h-[90vh] !w-full mx-auto" />
+    </section>
 </template>
 
 <script lang="ts">
-import customer from '../../../public/customers.json';
-import payment from '../../../public/payments.json';
-import product from '../../../public/products.json';
+import customer from "../../../public/customers.json";
+import payment from "../../../public/payments.json";
+import product from "../../../public/products.json";
+
+import { Line } from 'vue-chartjs'
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js'
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
 
 const resPayment = payment.map((item) => {
     return {
@@ -104,44 +140,64 @@ const resProduct = product.map((item) => {
 });
 
 export default {
-    name: 'data',
+    name: "data",
+    components: { Line },
     data() {
         return {
             customers: customer.data,
             selectedCustomer: null,
             payments: resPayment,
-            selectedPayment: null as { id: number, name: string } | null,
+            selectedPayment: null as { id: number; name: string } | null,
             products: resProduct,
-            selectedProduct: null as { id: number, name: string, price: number } | null,
+            selectedProduct: null as { id: number; name: string; price: number; } | null,
+
+            chartData: {
+                labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni'],
+                datasets: [{
+                    label: 'Data Per Tahun',
+                    backgroundColor: '#f87979',
+                    data: [251437000, 251437000, 754631000, 444912000, 159987000, 387547000],
+                }]
+            },
+            chartOptions: {
+                responsive: true,
+                type: Object,
+                default: () => { }
+            },
         };
     },
     methods: {
         // Format nilai menjadi mata uang
         formatCurrency(value: number, countryCode: string, currencyCode: string): string {
             return value.toLocaleString(`id-${countryCode}`, {
-                style: 'currency',
+                style: "currency",
                 currency: currencyCode,
             });
         },
+        // Get total dengan discount
+        // Discount di hitung per qty bukan total product
         calculateTotalWithDiscount(price: any, firstDiscount: any, secondDiscount: any) {
             // Konversi persentase diskon pertama ke desimal
             const firstDiscountDecimal = firstDiscount / 100;
-
             // Hitung nilai diskon pertama
             const firstDiscountAmount = price * firstDiscountDecimal;
-
             // Konversi persentase diskon kedua ke desimal
             const secondDiscountDecimal = secondDiscount / 100;
-
             // Hitung nilai diskon kedua
             const secondDiscountAmount = price * secondDiscountDecimal;
-
             // Hitung total setelah kedua diskon
             const totalPrice = price - firstDiscountAmount - secondDiscountAmount;
-
+            // Karena langsung mengembalikan NaN
+            if (Number.isNaN(totalPrice)) {
+                return 0;
+            }
             return totalPrice;
-        }
-    }
+        },
+        // Menambah atau Mengurangi jumlah produk
+        quantity(total: any, quantity: any) {
+            return total * quantity;
+        },
+    },
 };
 </script>
 
